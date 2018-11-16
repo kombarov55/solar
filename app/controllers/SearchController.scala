0785f34c5d3ba839e619635f5b2c6a092b98b5fa
Nikolay <kombarov55@gmail.com>
2018-11-16 14:16:04 +0300

    Добавил ws библиотеку Добавил сервис поиска тегов

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -3,14 +3,15 @@ package controllers
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 import play.api.libs.json._
+import service.TagService
 
-class SearchController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
+class SearchController @Inject()(private val tagService: TagService, cc: ControllerComponents) extends AbstractController(cc) {
 
   def search() = Action { implicit rq =>
     rq.uri
 
-    val json = """{ "result": "ok" }"""
-    Ok(Json.toJson(json))
+    val json = tagService.stub
+    Ok(Json.prettyPrint(Json.toJson(json)))
   }
 
 }
